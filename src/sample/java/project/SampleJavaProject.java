a0ee6906cd99386b3bc700d11e7891cb29c2b389
shar1810 <69445643+shar1810@users.noreply.github.com>
2026-02-20 11:44:16 +0530

    Update SampleJavaProject.java

--- src/sample/java/project/SampleJavaProject.java
+++ src/sample/java/project/SampleJavaProject.java
@@ -1,25 +1,21 @@
 package sample.java.project;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import com.sun.net.httpserver.HttpServer;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class SampleJavaProject {
+@WebServlet("/hello")
+public class SampleJavaProject extends HttpServlet {
 
-    public static void main(String[] args) throws IOException {
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
-
-        server.createContext("/", exchange -> {
-            String response = "Hello from Ant-built JAR Web App!";
-            exchange.sendResponseHeaders(200, response.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        });
-
-        server.start();
-        System.out.println("Server started at http://localhost:8081");
+        response.setContentType("text/html");
+        response.getWriter().println("<h2>Hello from JAR inside Tomcat!</h2>");
     }
 }
